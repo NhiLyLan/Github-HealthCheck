f279ae98949df5c7c4ec1d12054ae48f62432b4c
nhi.ly+kataloncloud@katalon.com <katalon_g3_dummy_username>
2025-06-25 05:03:29 +0000

    Edit "TS_RegressionTest - Data Binding" test suite

--- Test Suites/Data Binding/TS_RegressionTest - Data Binding.ts
+++ Test Suites/Data Binding/TS_RegressionTest - Data Binding.ts
@@ -58,4 +58,42 @@
          <variableId>9486406f-e769-4d84-bbfd-a0be7ea97976</variableId>
       </variableLink>
    </testCaseLink>
+   <testCaseLink>
+      <guid>0dc7fe71-c2f2-442b-b4b4-d54460759a45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases/Login Critical (1) (1) (1)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
+      </variableLinks>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
+      </variableLinks>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b5559c82-ded6-4214-9037-5d60a9e886c5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases/Login Critical (1)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
+      </variableLinks>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
+      </variableLinks>
+   </testCaseLink>
 </TestSuiteEntity>
