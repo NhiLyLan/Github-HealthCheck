46c22240259a26dc35a7ead4c15795cf2e9d6fdc
nhi.ly+kataloncloud@katalon.com <katalon_g3_dummy_username>
2025-07-15 15:20:46 +0000

    Delete 2 test case(s)

--- Test Suites/Regression Azure Link.ts
+++ Test Suites/Regression Azure Link.ts
@@ -11,18 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>6282b67c-c258-48fc-ae18-69de483732bc</testSuiteGuid>
-   <testCaseLink>
-      <guid>c415ccb1-daf6-43ef-83b3-628259923c15</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TestOps Visual Testing/Visual Testing - Verify Login Page (1) (1)</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>43e028af-8f57-490d-86a3-556325330240</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TestOps Visual Testing/Visual Testing - Verify Login Page (1)</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
