9fa94bcb1aa4844c81febf46d4c60edb2cbf43d1
nhi.ly+kataloncloud@katalon.com <katalon_g3_dummy_username>
2025-07-16 06:58:44 +0000

    Edit "Regression Azure Link" test suite

--- Test Suites/Regression Azure Link.ts
+++ Test Suites/Regression Azure Link.ts
@@ -11,4 +11,23 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>6282b67c-c258-48fc-ae18-69de483732bc</testSuiteGuid>
+   <testCaseLink>
+      <guid>2fbdc39a-cfc9-4b41-ae92-d57c78ca80fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1_Verify Successful Login - Has Azure Link - Valid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b4304c1f-1e35-468e-a2dc-8790496ebd77</variableId>
+      </variableLinks>
+      <variableLinks>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>366e3ca9-9280-4567-8602-0fdb3cf389c0</variableId>
+      </variableLinks>
+   </testCaseLink>
 </TestSuiteEntity>
