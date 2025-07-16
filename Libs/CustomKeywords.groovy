50f27dae97ef44eaf7013f964b512c869b186c4e
NhiLy <nhi.ly@katalon.com>
2025-07-16 14:47:16 +0700

    git delete

--- Libs/CustomKeywords.groovy
+++ Libs/CustomKeywords.groovy
@@ -14,7 +14,12 @@ import com.applitools.eyes.selenium.Eyes
 import com.applitools.eyes.RectangleSize
 
 
-
+ /**
+	 * Check if element present in timeout
+	 * @param to Katalon test object
+	 * @param timeout time to wait for element to show up 
+	 * @return true if element present, otherwise false
+	 */ 
 def static "com.example.WebUICustomKeywords.isElementPresent"(
     	TestObject to	
      , 	int timeout	) {
@@ -30,7 +35,12 @@ def static "com.example.WebUICustomKeywords.abc"(
         	a)
 }
 
-
+ /**
+	 * Get all rows of HTML table
+	 * @param table Katalon test object represent for HTML table
+	 * @param outerTagName outer tag name of TR tag, usually is TBODY
+	 * @return All rows inside HTML table 
+	 */ 
 def static "com.example.WebUICustomKeywords.getHtmlTableRows"(
     	TestObject table	
      , 	String outerTagName	) {
@@ -39,7 +49,12 @@ def static "com.example.WebUICustomKeywords.getHtmlTableRows"(
          , 	outerTagName)
 }
 
-
+ /**
+	 * Get all cells of HTML table row
+	 * @param row a WebElement instance represent for HTML table row 
+	 * @param tagName HTML column tag name, usually is TD/TH
+	 * @return All cells inside HTML table row 
+	 */ 
 def static "com.example.WebUICustomKeywords.getHtmlTableColumns"(
     	WebElement row	
      , 	String tagName	) {
