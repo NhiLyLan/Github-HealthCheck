31e3720706acc67eca16b68b370c05e5160527f5
cuong.vo3@katalon.com <katalon_g3_dummy_username>
2025-12-24 04:17:52 +0000

    Create "new suite from testops" test suite

---
+++ Test Suites/new suite from testops.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>new suite from testops</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>bfe45d16-ce6a-429f-a893-b8a8d0093ec1</testSuiteGuid>
+    <testCaseLink>
+        <guid>21698d99-db26-44cc-864b-20751146fc19</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/TestOps Visual Testing/Visual Testing - Verify Login Page</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+</TestSuiteEntity>
