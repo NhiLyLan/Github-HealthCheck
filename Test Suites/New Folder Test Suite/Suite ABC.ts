71aff66444171a9cae7b1d1df06238754a1665a9
NhiLy <nhi.ly@katalon.com>
2025-12-24 15:07:49 +0700

    COMMIT

---
+++ Test Suites/New Folder Test Suite/Suite ABC.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite ABC</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>402fd495-e54a-440d-b8f2-2a9a2dc5aa41</testSuiteGuid>
+   <testCaseLink>
+      <guid>e1e0549a-bdc3-40c5-8f0f-3bb4a9fa82bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOps Visual Testing/Folder TC From TOs/Visual Testing - Verify Login Page - Rename ABC</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9061ee0f-03fe-47ba-a312-8b9400ce45bd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOps Visual Testing/Folder TC From TOs/Visual Testing - Verify Login Page (1)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8fa67156-5c27-4118-aa42-94a7720f7f20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOps Visual Testing/Visual Testing - Verify Login Page - Rename</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9ade0be3-620a-4c6d-8583-85f9c55b85ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOps Visual Testing Folder KS/Visual Testing - Verify Login Page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ba29381-9e0c-4f8b-b137-2f9f7a07c7ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOps Visual Testing Folder KS/Visual Testing - Verify Login Page - Check webhook</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
--- Test Suites/new suite from testops.ts
+++ Test Suites/new suite from testops.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-   <name>new suite from testops</name>
    <description></description>
+   <name>new suite from testops</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -11,4 +11,23 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>bfe45d16-ce6a-429f-a893-b8a8d0093ec1</testSuiteGuid>
+   <testCaseLink>
+      <guid>548ae96a-afa6-4391-af94-5e594a56331e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AI-Generated/Fide/TC1-Explore Sustainable Investments and Sign Up for Updates</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a1b3497f-a57a-4fa0-a201-e90d018b9df9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>5fcae328-3026-4a78-9ed8-5ecde729e71a</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
