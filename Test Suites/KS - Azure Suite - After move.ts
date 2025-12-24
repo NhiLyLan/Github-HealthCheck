25462f4c8b92ae9ed4e64bafdeefc20d2da9cda8
NhiLy <92984121+NhiLyLan@users.noreply.github.com>
2025-12-24 11:17:06 +0700

    f

--- Test Suites/KS - Azure Suite - After move.ts
+++ Test Suites/KS - Azure Suite - After move.ts
@@ -1,11 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-<<<<<<<< HEAD:Test Suites/Full status/KS - Azure Suite - After move.ts
-   <name>KS - Azure Suite - After move</name>
-========
-   <name>TO Suite will execution in KS Rename 1</name>
->>>>>>>> master:Test Suites/TestSuiteFromTO/TO Suite will execution in KS Rename 1.ts
+   <name>KS - Azure Suite - After move - After move</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
