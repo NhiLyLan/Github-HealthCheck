6e0abf69cf6e6a232f72190f66f4cd6f8c76bd8a
NhiLy <nhi.ly@katalon.com>
2026-01-27 17:07:39 +0700

    ssfdhf

--- Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
+++ Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
@@ -12,7 +12,7 @@
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
             <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Firefox</runConfigurationId>
+            <runConfigurationId>Safari</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/TS_RegressionTest_Release_01</testSuiteEntity>
