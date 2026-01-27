c64940b2b4260b51805c73f253beb30c9dfea0a5
NhiLy <nhi.ly@katalon.com>
2026-01-27 18:45:53 +0700

    x

--- Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
+++ Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
@@ -10,7 +10,7 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
+            <profileName>New Profile</profileName>
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Safari</runConfigurationId>
          </configuration>
