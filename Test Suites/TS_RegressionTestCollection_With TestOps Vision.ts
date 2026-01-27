e2831015374c4ff825eaaf9ab974b421c5362556
NhiLy <nhi.ly@katalon.com>
2026-01-27 16:57:29 +0700

    chrome headlesss commit

--- Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
+++ Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
@@ -12,7 +12,7 @@
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
             <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Edge Chromium</runConfigurationId>
+            <runConfigurationId>Chrome (headless)</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/TS_RegressionTest_Release_01</testSuiteEntity>
