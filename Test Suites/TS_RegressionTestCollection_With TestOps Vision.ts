17787602cd2d7ba99ff34a982b54ec5a730812b3
NhiLy <nhi.ly@katalon.com>
2026-01-27 16:29:59 +0700

    change to FF

--- Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
+++ Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
@@ -12,7 +12,7 @@
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
             <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
+            <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/TS_RegressionTest_Release_01</testSuiteEntity>
