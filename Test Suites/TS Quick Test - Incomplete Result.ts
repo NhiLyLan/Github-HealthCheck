346b0a7be74a3f08ea6f9657d29cb38c59385168
NhiLy <nhi.ly@katalon.com>
2026-01-28 11:18:28 +0700

    commit change

---
+++ Test Suites/TS Quick Test - Incomplete Result.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Quick Test - Incomplete Result</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9823067b-2ff6-4cfc-bd8b-c9a1555907dc</testSuiteGuid>
+   <testCaseLink>
+      <guid>f83146b1-4112-4bd2-89a1-99dd98b88137</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/Quick Test Incomplete</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
