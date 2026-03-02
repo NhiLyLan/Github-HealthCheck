6625865e3bd67020a27d3c7d7d13f75b293a2454
NhiLy <nhi.ly@katalon.com>
2026-03-02 15:07:51 +0700

    comment 11

---
+++ Test Suites/Test Suite Folder/TS Quick Test - passed 11.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Quick Test - passed 11</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fc8c2f4a-cd5b-4798-b7b6-03aa17516a86</testSuiteGuid>
+   <testCaseLink>
+      <guid>0f75bfe9-315b-44c0-ae7f-6e893d3b8388</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/Quick Test Passed</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
