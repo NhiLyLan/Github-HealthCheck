e480c3ced4b5671df607aa7bd7eb3715b2ef5584
NhiLy <nhi.ly@katalon.com>
2026-03-04 15:21:16 +0700

    change env

--- Test Suites/TSC Folder/TSC Quick Test - Not Change.ts
+++ Test Suites/TSC Folder/TSC Quick Test - Not Change.ts
@@ -46,24 +46,30 @@
             <requireConfigurationData>true</requireConfigurationData>
             <runConfigurationData>
                <entry>
-                  <key>testCloudDesktopBrowser</key>
+                  <key>testCloudMobileDevice</key>
                   <value>{
-  &quot;id&quot;: 1949,
-  &quot;osDisplayName&quot;: &quot;Linux&quot;,
-  &quot;browserName&quot;: &quot;chrome&quot;,
-  &quot;browserDisplayName&quot;: &quot;Chrome&quot;,
-  &quot;browserVersion&quot;: &quot;145&quot;,
-  &quot;browserVersionType&quot;: &quot;stable&quot;,
-  &quot;os&quot;: &quot;linux&quot;,
+  &quot;capability&quot;: {
+    &quot;availability&quot;: &quot;HIGH&quot;
+  },
+  &quot;id&quot;: &quot;apple_iphone_17&quot;,
+  &quot;isTablet&quot;: false,
+  &quot;manufacturer&quot;: [
+    &quot;Apple&quot;
+  ],
+  &quot;mobileDeviceId&quot;: &quot;iPhone_17_ios_26&quot;,
+  &quot;name&quot;: &quot;Apple iPhone 17&quot;,
+  &quot;displayName&quot;: &quot;Apple iPhone 17&quot;,
+  &quot;osVersion&quot;: &quot;26&quot;,
+  &quot;os&quot;: &quot;iOS&quot;,
   &quot;platformName&quot;: &quot;IOS&quot;,
   &quot;timeout&quot;: 15,
   &quot;usingTunnel&quot;: false,
-  &quot;isHighAvailableDevice&quot;: false
+  &quot;isHighAvailableDevice&quot;: true
 }</value>
                </entry>
                <entry>
                   <key>testCloudSessionType</key>
-                  <value>DESKTOP_BROWSER</value>
+                  <value>MOBILE_BROWSER</value>
                </entry>
             </runConfigurationData>
             <runConfigurationId>TestCloud</runConfigurationId>
