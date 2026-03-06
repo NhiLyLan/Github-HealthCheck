02a73fd00fd87c9e9795fb5fd50f4f652b372bcd
NhiLy <nhi.ly@katalon.com>
2026-03-06 11:03:44 +0700

    commit

---
+++ Test Suites/Test Suite Folder/TS Quick Test - passed check.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Quick Test - passed check</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ab45bc4a-22e3-40f6-8c14-7453b096d3fe</testSuiteGuid>
+   <testCaseLink>
+      <guid>0f75bfe9-315b-44c0-ae7f-6e893d3b8388</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases/Quick Test Passed</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
