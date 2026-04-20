fc3e96e5bb73a57360a84e5933c2718de9923ebf
NhiLy <nhi.ly@katalon.com>
2026-04-20 08:31:27 +0700

    merge

---
+++ Test Suites/Suite Visual Testing.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite Visual Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cf75e84e-5838-4057-9670-60756058fcd8</testSuiteGuid>
+   <testCaseLink>
+      <guid>8b7b75cd-3e83-429b-8be4-36aaf5c85679</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases/Login - VT</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>01f48cdb-919e-4870-9802-cdfb4a6c1428</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>091068f3-5d16-41e5-a0ce-d096747be52a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
