b77b786810492a133c5e395351e840284dcc0a32
NhiLy <nhi.ly@katalon.com>
2026-04-20 09:13:24 +0700

    ignored zone

---
+++ Test Suites/Suite Visual Testing - Incorrect Text.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite Visual Testing - Incorrect Text</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>93b6d549-f59a-4483-8a6d-a805264ad885</testSuiteGuid>
+   <testCaseLink>
+      <guid>8b7b75cd-3e83-429b-8be4-36aaf5c85679</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common Test Cases/Login - VT - Incorrect Text</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c09b14a0-0b38-4465-8da2-2681adda0170</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3a45d586-4b45-4c58-a862-509926177bdc</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
